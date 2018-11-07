26a452118c454b7217ef3ebecf4b0dc1052acaee
Grigorii Kislyakov <grigorij.kislyakov@yandex.ru>
2018-11-07 23:03:46 +0300

    First version of DirectionBuilder

---
+++ Builder/DirectionBuilder/src/main/java/com/example/demo/weather_app/DirectionBuilder/error/MassSizeException.java
@@ -0,0 +1,7 @@
+package com.example.demo.weather_app.DirectionBuilder.error;
+
+public class MassSizeException extends ArrayIndexOutOfBoundsException {
+    public MassSizeException(String s) {
+        super(s);
+    }
+}
