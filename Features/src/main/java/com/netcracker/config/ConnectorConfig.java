37111ae1bd6953d41cb0298c33cd3805ecb714bb
prokhorovartem <art.98.pro@gmail.com>
2018-11-13 00:27:10 +0300

    fixed styles for first request

--- Features/src/main/java/com/netcracker/config/ConnectorConfig.java
+++ Features/src/main/java/com/netcracker/config/ConnectorConfig.java
@@ -33,7 +33,7 @@ public class ConnectorConfig {
     private Connector redirectConnector() {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
-        connector.setPort(8080);
+        connector.setPort(8081);
         connector.setSecure(false);
         connector.setRedirectPort(8443);
         return connector;
--- Features/src/main/java/com/netcracker/config/SecurityConfig.java
+++ Features/src/main/java/com/netcracker/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/loginFailure", "/")
+                .antMatchers("/loginFailure", "/", "/aquamarine.css", "/assets/image/original.jpg", "/js/*")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
