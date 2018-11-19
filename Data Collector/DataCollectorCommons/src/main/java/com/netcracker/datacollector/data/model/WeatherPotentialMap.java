b5f74feaf5afc10645455c5e86c0270bd9b8d214
Kirill Vakhrushev <kirill.vakhrushev@gmail.com>
2018-11-19 18:20:11 +0300

    Fix weather collecotor

--- Data Collector/DataCollectorCommons/src/main/java/com/netcracker/datacollector/data/model/WeatherPotentialMap.java
+++ Data Collector/DataCollectorCommons/src/main/java/com/netcracker/datacollector/data/model/WeatherPotentialMap.java
@@ -1,53 +1,23 @@
 package com.netcracker.datacollector.data.model;
 
-import lombok.*;
-import org.hibernate.annotations.Proxy;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
 
 @Entity
 @NoArgsConstructor
-@Table(name = "weatherMap")
 @Data
-// daba смысл этой аннотации? у сущности нет связей вообще
-@Proxy(lazy=false)
 public class WeatherPotentialMap {
 
     @Id
-    @NonNull
     private int id;
 
-    @NonNull
-    @Column(name = "scale")
     int scale;
 
     @Lob
-    @NonNull
-    @Column(name = "field")
     int[][] potentialField;
 
-    // daba ломбок сюда, @Data уже есть
-    public int[][] getPotentialField() {
-        return potentialField;
-    }
-
-    public void setPotentialField(int[][] potentialField) {
-        this.potentialField = potentialField;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setScale(int scale) {
-        this.id = scale;
-    }
-
-    public int getScale() {
-        return id;
-    }
 }
--- Data Collector/DataCollectorCommons/src/main/java/com/netcracker/datacollector/data/repository/WeatherPotentialMapRepository.java
+++ Data Collector/DataCollectorCommons/src/main/java/com/netcracker/datacollector/data/repository/WeatherPotentialMapRepository.java
@@ -2,12 +2,17 @@ package com.netcracker.datacollector.data.repository;
 
 import com.netcracker.datacollector.data.model.WeatherPotentialMap;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+@Repository
 public interface WeatherPotentialMapRepository extends JpaRepository<WeatherPotentialMap, Integer> {
 
-    /*
-    @Modifying(clearAutomatically = true)
-    @Query("Update WeatherPotentialMap wMap SET wMap.potentialField=:potentialField WHERE wMap.id=:id")
-    public void updateMap(@Param("id") int id, @Param("potentialField") int[][] potentialField);
-    */
+    @Transactional
+    @Modifying
+    @Query("UPDATE WeatherPotentialMap m SET m.potentialField = :map WHERE m.id = :mapId")
+    void updateAddress(@Param("map") int[][] map, @Param("mapId") int mapId);
+
 }
