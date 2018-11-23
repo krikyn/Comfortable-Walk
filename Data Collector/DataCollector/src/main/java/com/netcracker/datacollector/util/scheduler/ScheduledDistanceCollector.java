3ac90a711a3f43deeedc15136a780c5e39ba3014
prokhorovartem <art.98.pro@gmail.com>
2018-11-24 02:01:19 +0300

    added alternative version to save distances from google

--- Data Collector/DataCollector/src/main/java/com/netcracker/datacollector/util/scheduler/ScheduledDistanceCollector.java
+++ Data Collector/DataCollector/src/main/java/com/netcracker/datacollector/util/scheduler/ScheduledDistanceCollector.java
@@ -22,7 +22,9 @@ import java.util.Map;
 @Service
 public class ScheduledDistanceCollector {
 
-    private final int AMOUNT_OF_POINTS = 1560;
+    private final int AMOUNT_OF_POINTS = 1599;
+    private final int HORIZONTALLY_POINTS = 39;
+    private final int VERTICALLY_POINTS = 41;
     private final int AMOUNT_OF_DESTINATIONS_PER_REQUEST = 25;
 
     private final DistanceRepository distanceRepository;
@@ -33,8 +35,46 @@ public class ScheduledDistanceCollector {
 
 //    ОПАСНО! НЕ СНИМАТЬ!
 //    @Scheduled(fixedDelay = 1)
-    public void saveDistances() {
-        try (FileReader reader = new FileReader("distanceCounter.yaml")) {
+//    public void saveDistances() {
+//        try (FileReader reader = new FileReader("distanceCounter.yaml")) {
+//            Map<String, Integer> loadedData = yaml.load(reader);
+//            fromPointCounter = loadedData.get("fromPointCounter");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        ArrayList<String> destinations = distanceUtil.findDestinations();
+//        String[] arrayOfDestinations = destinations.toArray(new String[0]);
+//        for (int fromPoint = fromPointCounter; fromPoint < AMOUNT_OF_POINTS; fromPoint++) {
+//
+//            for (int j = 0; j <= arrayOfDestinations.length; j += AMOUNT_OF_DESTINATIONS_PER_REQUEST) {
+//
+//                String[] shortenedArray = Arrays.copyOfRange(arrayOfDestinations, j, j + AMOUNT_OF_DESTINATIONS_PER_REQUEST);
+//                DistanceMatrixElement[] distanceMatrixElements = new DistanceMatrixElement[0];
+//                try {
+//                    distanceMatrixElements = distanceUtil.getDistance(arrayOfDestinations[fromPoint], shortenedArray);
+//                } catch (InterruptedException | ApiException | IOException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                for (int i = 0; i < distanceMatrixElements.length; i++) {
+//                    if (i + j >= AMOUNT_OF_POINTS) break;
+//                    saveDistance(fromPoint, i + j, distanceMatrixElements[i]);
+//                }
+//            }
+//            try (FileWriter writer = new FileWriter("distanceCounter.yaml")) {
+//                Map<String, Integer> counter = new HashMap<>(); //Сохраняем значения счётчиков в yaml файл
+//                counter.put("fromPointCounter", fromPoint + 1);
+//                yaml.dump(counter, writer);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+
+//    @Scheduled(fixedDelay = 1000000000)
+    public void saveDistancesWithLinks() {
+       try (FileReader reader = new FileReader("distanceCounter.yaml")) {
             Map<String, Integer> loadedData = yaml.load(reader);
             fromPointCounter = loadedData.get("fromPointCounter");
         } catch (IOException e) {
@@ -43,26 +83,57 @@ public class ScheduledDistanceCollector {
 
         ArrayList<String> destinations = distanceUtil.findDestinations();
         String[] arrayOfDestinations = destinations.toArray(new String[0]);
-        for (int fromPoint = fromPointCounter; fromPoint < AMOUNT_OF_POINTS; fromPoint++) {
-
-            for (int j = 0; j <= arrayOfDestinations.length; j += AMOUNT_OF_DESTINATIONS_PER_REQUEST) {
-
-                String[] shortenedArray = Arrays.copyOfRange(arrayOfDestinations, j, j + AMOUNT_OF_DESTINATIONS_PER_REQUEST);
-                DistanceMatrixElement[] distanceMatrixElements = new DistanceMatrixElement[0];
-                try {
-                    distanceMatrixElements = distanceUtil.getDistance(arrayOfDestinations[fromPoint], shortenedArray);
-                } catch (InterruptedException | ApiException | IOException e) {
-                    e.printStackTrace();
+        for (int i = fromPointCounter; i < arrayOfDestinations.length; i++) {
+            DistanceMatrixElement[] distanceMatrixElements = new DistanceMatrixElement[0];
+            String[] destinationsArray = new String[4];
+            try {
+                //если начало в последнем квадрате
+                if (i == arrayOfDestinations.length - 1)
+                    break;
+                //путь в правую точку
+                destinationsArray[0] = arrayOfDestinations[i + 1];
+                //если начало в последней строчке
+                if (i >= AMOUNT_OF_POINTS - HORIZONTALLY_POINTS) {
+                    String[] lastRowDestinationsArray = {destinationsArray[0]};
+                    distanceMatrixElements = distanceUtil.getDistance(arrayOfDestinations[i], lastRowDestinationsArray);
+                    saveDistance(i, i + 1, distanceMatrixElements[0]);
+                    continue;
                 }
-
-                for (int i = 0; i < distanceMatrixElements.length; i++) {
-                    if (i + j >= AMOUNT_OF_POINTS) break;
-                    saveDistance(fromPoint, i + j, distanceMatrixElements[i]);
+                //путь в левую нижнюю точку
+                destinationsArray[1] = arrayOfDestinations[i + HORIZONTALLY_POINTS - 1];
+                //путь в нижнюю точку
+                destinationsArray[2] = arrayOfDestinations[i + HORIZONTALLY_POINTS];
+                //если начало в крайней правой колонке
+                if (i % HORIZONTALLY_POINTS == 38) {
+                    String[] rightColumnDestinationsArray = {destinationsArray[1], destinationsArray[2]};
+                    distanceMatrixElements = distanceUtil.getDistance(arrayOfDestinations[i], rightColumnDestinationsArray);
+                    saveDistance(i, i + HORIZONTALLY_POINTS - 1, distanceMatrixElements[0]);
+                    saveDistance(i, i + HORIZONTALLY_POINTS, distanceMatrixElements[1]);
+                    continue;
                 }
+                //путь в правую нижнюю точку
+                destinationsArray[3] = arrayOfDestinations[i + HORIZONTALLY_POINTS + 1];
+                //если начало в крайней левой колонке
+                if (i % HORIZONTALLY_POINTS == 0) {
+                    String[] leftColumnDestinationsArray = {destinationsArray[0], destinationsArray[2], destinationsArray[3]};
+                    distanceMatrixElements = distanceUtil.getDistance(arrayOfDestinations[i], leftColumnDestinationsArray);
+                    saveDistance(i, i + 1, distanceMatrixElements[0]);
+                    saveDistance(i, i + HORIZONTALLY_POINTS, distanceMatrixElements[1]);
+                    saveDistance(i, i + HORIZONTALLY_POINTS + 1, distanceMatrixElements[2]);
+                    continue;
+                }
+                distanceMatrixElements = distanceUtil.getDistance(arrayOfDestinations[i], destinationsArray);
+            } catch (InterruptedException | ApiException | IOException e) {
+                e.printStackTrace();
             }
+            saveDistance(i, i + 1, distanceMatrixElements[0]);
+            saveDistance(i, i + HORIZONTALLY_POINTS - 1, distanceMatrixElements[1]);
+            saveDistance(i, i + HORIZONTALLY_POINTS, distanceMatrixElements[2]);
+            saveDistance(i, i + HORIZONTALLY_POINTS + 1, distanceMatrixElements[3]);
+
             try (FileWriter writer = new FileWriter("distanceCounter.yaml")) {
                 Map<String, Integer> counter = new HashMap<>(); //Сохраняем значения счётчиков в yaml файл
-                counter.put("fromPointCounter", fromPoint + 1);
+                counter.put("fromPointCounter", i + 1);
                 yaml.dump(counter, writer);
             } catch (IOException e) {
                 e.printStackTrace();
