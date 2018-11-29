5e0bf4554804c79df787715c0c72c025736643c2
prokhorovartem <art.98.pro@gmail.com>
2018-11-30 01:21:09 +0300

    added some validation to the frontend side

--- Features/src/main/java/com/netcracker/controller/MainPageController.java
+++ Features/src/main/java/com/netcracker/controller/MainPageController.java
@@ -15,7 +15,10 @@ public class MainPageController {
 
     @PostMapping("/loginSuccess")
     public String postParams(@ModelAttribute Path path) {
-        path.setCheckedItems(checkedItemsUtil.getItems(path.getPlaceName().toUpperCase()));
+        if (path.getPlaceName() == null)
+            path.setCheckedItems(null);
+        else
+            path.setCheckedItems(checkedItemsUtil.getItems(path.getPlaceName().toUpperCase()));
         System.out.println("From point: " + path.getFromPointLat() + ", " + path.getFromPointLng() + " To Point: "
                 + path.getToPointLat() + ", " + path.getToPointLng() + " Wanna best weather? "
                 + path.getIsBestWeather() + " Checked items: " + path.getCheckedItems());
