f36c638c855f51ed6bce3973b463c1c7c8d8acfd
prokhorovartem <art.98.pro@gmail.com>
2018-11-30 01:58:19 +0300

    only places from Russia available in autocomplete

--- Features/src/main/resources/static/js/script.js
+++ Features/src/main/resources/static/js/script.js
@@ -1,40 +1,31 @@
-var placeSearch, autocompleteFrom, autocompleteTo;
+var autocompleteFrom, autocompleteTo;
 
 function initAutocomplete() {
+    var defaultBounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(60.02781, 30.18035),
+        new google.maps.LatLng(59.84802, 30.52101));
+    var options = {
+        bounds: defaultBounds,
+        types: ['geocode'],
+        componentRestrictions: {country: 'ru'}
+    };
     autocompleteFrom = new google.maps.places.Autocomplete(
-        (document.getElementById('autocompleteFrom')),
-        {types: ['geocode']});
-    autocompleteFrom.addListener('place_changed', fillInAddress);
+        (document.getElementById('autocompleteFrom')), options);
+    autocompleteFrom.addListener('place_changed', fillInAddressFrom);
+
     autocompleteTo = new google.maps.places.Autocomplete(
-        (document.getElementById('autocompleteTo')),
-        {types: ['geocode']});
-    autocompleteTo.addListener('place_changed', fillInAddress);
+        (document.getElementById('autocompleteTo')), options);
+    autocompleteTo.addListener('place_changed', fillInAddressTo);
 }
 
-function fillInAddress() {
-    // Get the place details from the autocompleteFrom object.
+function fillInAddressFrom() {
     var placeFrom = autocompleteFrom.getPlace();
-    var placeTo = autocompleteTo.getPlace();
-
     document.getElementById('fromPointLat').value = placeFrom.geometry.location.lat(0);
     document.getElementById('fromPointLng').value = placeFrom.geometry.location.lng(0);
-    document.getElementById('toPointLat').value = placeTo.geometry.location.lat(0);
-    document.getElementById('toPointLng').value = placeTo.geometry.location.lng(0);
 }
 
-function geolocate() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var geolocation = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-            var circle = new google.maps.Circle({
-                center: geolocation,
-                radius: position.coords.accuracy
-            });
-            autocompleteFrom.setBounds(circle.getBounds());
-            autocompleteTo.setBounds(circle.getBounds());
-        });
-    }
+function fillInAddressTo() {
+    var placeTo = autocompleteTo.getPlace();
+    document.getElementById('toPointLat').value = placeTo.geometry.location.lat(0);
+    document.getElementById('toPointLng').value = placeTo.geometry.location.lng(0);
 }
